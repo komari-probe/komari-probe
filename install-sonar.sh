3d78f6e0742d32d4075e28e06fc72cffe5a63650
Steve <qingyashizi@gmail.com>
2026-09-23 21:49:03 +0800

    fix: resolve server release asset name in installer

--- install-sonar.sh
+++ install-sonar.sh
@@ -900,6 +900,7 @@ install_dependencies() {
 # Get download URL based on channel
 get_download_url() {
     local arch=$1
+    local file_name="sonar-linux-${arch}"
     local target_ver="${VERSION:-${SONAR_VERSION:-${KOMARI_VERSION}:-}}"
     if [ -n "$target_ver" ]; then
         echo "https://github.com/${REPO}/releases/download/${target_ver}/${file_name}"
